c8545f43acaa0f9e997646a55c88dc24493e9a4e
laptop <2584944133@qq.com>
2025-01-26 23:07:24 +0800

    add probability evaluate metrics

--- utils/metrics.py
+++ utils/metrics.py
@@ -46,10 +46,10 @@ def CRPS(observations, forecasts):
     """
     An alternative but simpler implementation of CRPS for testing purposes
     input:
-        observations: np.array of shape (n_timesteps,)
-        forecasts: np.array of shape (n_timesteps, n_samples)
+        observations: np.array of shape (n_observation, n_timesteps)
+        forecasts: np.array of shape (n_samples, n_observation, n_timesteps)
     output:
-        crps: np.array of shape (n_timesteps,)
+        crps: float
 
     This implementation is based on the identity:
 
@@ -73,17 +73,17 @@ def CRPS(observations, forecasts):
     forecasts = np.asarray(forecasts)
 
     if observations.ndim == forecasts.ndim - 1:
-        # sum over the last axis
-        assert observations.shape == forecasts.shape[:-1]
-        observations = observations[..., np.newaxis]
+        assert observations.shape == forecasts.shape[1:]
+        observations = observations[np.newaxis, ...]
 
-        score = np.nanmean(abs(forecasts - observations), -1)
-        # insert new axes along last and second to last forecast dimensions so
-        # forecasts_diff expands with the array broadcasting
-        forecasts_diff = (np.expand_dims(forecasts, -1) - np.expand_dims(forecasts, -2))
+        mae_score = np.nanmean(abs(forecasts - observations))
 
-        diff_score = np.nanmean(abs(forecasts_diff), axis=(-2, -1))
-        crps_value = score - 0.5 * diff_score
+        forecasts_diff = (np.expand_dims(forecasts, 0) - np.expand_dims(forecasts, 1))
+
+        diff_score = np.nanmean(abs(forecasts_diff))
+
+        crps_value = mae_score - 0.5 * diff_score
+
         return crps_value
     elif observations.ndim == forecasts.ndim:
         # there is no 'realization' axis to sum over (this is a deterministic
@@ -91,22 +91,20 @@ def CRPS(observations, forecasts):
         return abs(observations - forecasts)
 
 
-import numpy as np
-
 
 def ES(observations, forecasts):
     """
     Energy Score (ES) implementation
     input:
-        observations: np.array of shape (n_timesteps, n_features)
-        forecasts: np.array of shape (n_timesteps, n_samples, n_features)
+        observations: np.array of shape (n_observation, n_timesteps)
+        forecasts: np.array of shape (n_samples, n_observation, n_timesteps)
     output:
         es: np.array of shape (n_timesteps,)
 
     This implementation is based on the identity:
 
     .. math::
-        ES(F, x) = E_F[ ||X - x||_2^2 ] - 1/2 * E_F[ ||X - X'||_2^2 ]
+        ES(F, x) = E_F[ ||X - x||_2 ] - 1/2 * E_F[ ||X - X'||_2 ]
 
     where X and X' are independent random variables drawn from the forecast distribution F.
     """
@@ -115,15 +113,16 @@ def ES(observations, forecasts):
 
     if observations.ndim == forecasts.ndim - 1:
         # sum over the last axis if observations is one less dimension than forecasts
-        assert observations.shape == forecasts.shape[:-2]
-        observations = observations[..., np.newaxis]
+        assert observations.shape == forecasts.shape[1:]
+        observations = observations[np.newaxis, ...]
 
-        # Calculate the first expectation E_F[ ||X - Y||_2^2 ]
-        score = np.nanmean(np.linalg.norm(forecasts - observations, axis=-1) ** 2, -1)
+        # Calculate the first expectation E_F[ ||X - Y||_2 ]
+        X_Y = forecasts - observations
+        score = np.nanmean(np.linalg.norm(X_Y.flatten()))
 
-        # Calculate the second expectation E_F[ ||X - X'||_2^2 ]
-        forecasts_diff = np.expand_dims(forecasts, -2) - np.expand_dims(forecasts, -3)
-        diff_score = np.nanmean(np.linalg.norm(forecasts_diff, axis=-1) ** 2, axis=(-2, -1))
+        # Calculate the second expectation E_F[ ||X - X'||_2 ]
+        forecasts_diff = np.expand_dims(forecasts, 0) - np.expand_dims(forecasts, 1)
+        diff_score = np.nanmean(np.linalg.norm(forecasts_diff.flatten()))
 
         # Calculate the final Energy Score
         es_value = score - 0.5 * diff_score
@@ -131,17 +130,57 @@ def ES(observations, forecasts):
 
     elif observations.ndim == forecasts.ndim:
         # Handle the case where there is no "realization" axis (deterministic forecast)
-        return np.linalg.norm(observations - forecasts, axis=-1) ** 2
+        return np.linalg.norm(observations - forecasts)
 
 
+def VS(observations, forecasts):
+    """
+    An alternative but simpler implementation of CRPS for testing purposes
+    input:
+        observations: np.array of shape (n_observation, n_timesteps)
+        forecasts: np.array of shape (n_samples, n_observation, n_timesteps)
+    output:
+        crps: float
+
+    This implementation is based on the identity:
+
+    .. math::
+        VS(F, x) = E_F|X - x| - 1/2 * E_F|X - X'|
+
+    where X and X' denote independent random variables drawn from the forecast
+    distribution F, and E_F denotes the expectation value under F.
+
+    Hence it has runtime O(n^2) instead of O(n log(n)) where n is the number of
+    ensemble members.
+
+    Reference
+    ---------
+    Tilmann Gneiting and Adrian E. Raftery. Strictly proper scoring rules,
+        prediction, and estimation, 2005. University of Washington Department of
+        Statistics Technical Report no. 463R.
+        https://www.stat.washington.edu/research/reports/2004/tr463R.pdf
+    """
+    observations = np.asarray(observations)
+    forecasts = np.asarray(forecasts)
+    samples = forecasts.shape[0]
+    n_observations = forecasts.shape[1]
+    timesteps = forecasts.shape[2]
+    obser_diff_mat = abs(np.expand_dims(observations, 1) - np.expand_dims(observations, 2)) ** 0.5
+    forec_diff_mat = abs(np.expand_dims(forecasts, -1) - np.expand_dims(forecasts, -2)) ** 0.5
+    forec_diff_mat_mean = np.mean(forec_diff_mat, axis=0)
+    temp = (obser_diff_mat - forec_diff_mat_mean) ** 2
+    vs_value = np.mean(temp.flatten())
+    return vs_value
 
 
 
 if __name__ == '__main__':
     # 模拟一些预测数据和观测数据
-    forecasts = np.array([[0.5, 0.7, 0.6], [0.8, 0.7, 1]])
-    observations = np.array([0.6, 0.8])
+    fix_seed = 2024
+    np.random.seed(fix_seed)
+    observations = np.random.randint(0, 101, size=(3, 2))
+    forecasts = np.random.randint(0, 101, size=(4, 3, 2))
 
     # 计算 CRPS
-    crps_value = CRPS(observations, forecasts)
+    crps_value = VS(observations, forecasts)
     print(crps_value)
